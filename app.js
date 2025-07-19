739db8918ec583d4a3d3e4559f1f106523c0dc6c
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-07-19 21:49:49 +0200

    database: insall mongoose.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const api = require("./routes/api");
 const index = require("./routes");
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set("view engine", "pug ");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
