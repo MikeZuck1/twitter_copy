c61c28fa90589f00aca271481a3a41ec651e97e6
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-07-24 15:20:10 +0200

    mongoose: worked with validators.

--- database/models/chapters.model.js
+++ database/models/chapters.model.js
@@ -3,7 +3,17 @@ const schema = mongoose.Schema;
 
 const chapterSchema = new schema(
   {
-    title: String,
+    title: {
+      type: {
+        type: String,
+        required: true,
+        enum: ["chapter", "section", "sub-section"], // check if field match with the array values.
+        trim: true,
+      },
+      required: [true, "title is required..."],
+      minlength: [3, "very short title"],
+      maxlength: [10, "long title.. must be less than 10..."],
+    },
     nbrOfLessons: { type: Number, require: true },
     index: Number,
     active: Boolean,
