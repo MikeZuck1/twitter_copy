55e28e651d61f84c0cbb577b2fce5d7c3cfe23c3
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-07-29 00:04:38 +0200

    structure: restructured the project.

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const path = require("path");
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
-require("./database"); // Import the database connection
-const routing = require("./routes"); // check by default index.js
-const port = process.env.PORT;
+const path = require("path");
+const app = express();
+const index = require("./routes/index");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "./views"));
-app.set("view engine", "pug ");
+app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("tiny"));
+app.use(morgan("combined"));
 // define our routes.
-app.use(routing); 
+app.use(index); // put index instead of routing -- remove routing
 
 app.listen(port);
